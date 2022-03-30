c3c133fcaafbb090b3fca61555541d4f39c372c8
Aldidwikusuma <aldidwik518@gmail.com>
2022-03-30 23:45:05 +0700

    Membuat Controller dan View

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\Mahasiswa as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model; //Model Eloquent
 
 class Mahasiswa extends Model
 {
-    use HasFactory;
+    protected $table='mahasiswa'; // Eloquent akan membuat model mahasiswa menyimpan record di tabel mahasiswa
+    protected $primaryKey = 'id_mahasiswa'; // Memanggil isi DB Dengan primarykey
+
+    protected $fillable = [
+    'Nim',
+    'Nama',
+    'Kelas',
+    'Jurusan',
+    ];
 }
